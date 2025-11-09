abbdaaf6e09c052d1e48c8327c004e2237aa6764
tharun11 <47290042+DrCheeseFace@users.noreply.github.com>
2025-11-09 20:13:40 +0000

    feat: debugging script

--- debug_qemu.sh
+++ debug_qemu.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
+
 set -e
 . ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -kernel kernel/pissos.kernel -s -S
+GDB_BIN="i686-elf-gdb"
+GDB_WINDOW="gdb"
+KERNEL_FILE="kernel/pissos.kernel"
+GDB_INIT_FILE=".gdbinit"
+
+GDB_TARGET_PANE="${GDB_WINDOW}.1"
+
+QEMU_CMD="qemu-system-$(./target-triplet-to-arch.sh $HOST) \
+  -kernel ${KERNEL_FILE} \
+  -s -S"
+
+GDB_COMMAND="${GDB_BIN}"
+
+
+tmux select-window -t ":${GDB_WINDOW}"
+
+tmux send-keys -t ${GDB_TARGET_PANE} 'clear' Enter
+
+tmux send-keys -t ${GDB_TARGET_PANE} "${GDB_COMMAND}" Enter
+
+exec $QEMU_CMD
